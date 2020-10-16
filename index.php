4740099f3cbb6080d2f0c981cbe4d203d382d515
papalmt <72967155+papalmt@users.noreply.github.com>
2020-10-16 15:13:02 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 
 
 $API_URL = 'https://api.line.me/v2/bot/message';
-$ACCESS_TOKEN = 'ouhqskdRP/sUP8uwpjAadPDJz6rj1Y3IR0/ZznmHBgsPmYq6Q+hzdEJ4OXgyw/8NaLy6GLAZYYbLhF/7S6i8K07k3yxT0sWcMEa6ixgJ2c0XIOEKRfUEQAsHVi4PbQU4HEk9GOq/cmdR3iRkQE9e5gdB04t89/1O/w1cDnyilFU='; 
-$channelSecret = 'ba6e01c3eb0671a32e7d9fb3dbabd67d';
+$ACCESS_TOKEN = 'nQ4hVbk6EfzUoKyKBtjc0DvEFOW7a6j11u4Ugn5wySMT9omlK7J+2+NB6j1Z0LS20kJOT9ks9CB9m3uNV7R5LjHpoF4o2b2nNcMHEafRA3asczdjMOuSNO418IzoapRIUV8cmqtw9MQW6nloiDx7JQdB04t89/1O/w1cDnyilFU='; 
+$channelSecret = '36e8e53d84555ff6c721873e667ccae4';
 
 
 $POST_HEADER = array('Content-Type: application/json', 'Authorization: Bearer ' . $ACCESS_TOKEN);
